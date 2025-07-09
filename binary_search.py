b1c561b6e032e7ac98d99591d4be5d488bdf53ff
Jagadishwar49 <73735680+Jagadishwar49@users.noreply.github.com>
2025-07-09 20:45:48 +0530

    Update binary_search.py

--- binary_search.py
+++ binary_search.py
@@ -1,12 +1,14 @@
+print ('Hello World')
 def binarySearch(l,a,b,v):
     mid = int((a+b)//2)
-    if(mid<=0):
+    if(mid<0):
         return(False)
     if v==l[mid]:
         return(True)
-    elif v > l[mid]:
-        return binarySearch(l,mid+1,b,v)
-    else:
+    elif v < l[mid]:
         return binarySearch(l,a,mid,v)
+    else:
+        return binarySearch(l,mid,b,v)
 
-print(binarySearch([1,2,3,4,5],0,6,1))
+a=[1,2,3,4,5]
+print(binarySearch(a,0,len(a),1))
